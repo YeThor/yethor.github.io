6098f39c2f75fdb5dd7fed1cf3d9ea92c7c961e7
YESOL KIM <kysloves@naver.com>
2023-04-03 00:29:44 +0900

    :sparkles: make tags page to show same UI in index page

--- src/templates/tags.tsx
+++ src/templates/tags.tsx
@@ -1,30 +1,29 @@
 import React from "react";
 
 import { Link, graphql } from "gatsby";
+import PostListItem from "../components/PostListItem";
+import { Layout } from "../components";
 
-const Tags = ({ pageContext, data }) => {
+const Tags = ({ pageContext, data, location }) => {
   const { tag } = pageContext;
-  const { edges, totalCount } = data.allMarkdownRemark;
+  const { totalCount, nodes: posts } = data.allMarkdownRemark;
+
+  console.log(posts, data.allMarkdownRemark);
+
   const tagHeader = `${totalCount} post${
     totalCount === 1 ? "" : `s`
   } tagged with "${tag}"`;
 
   return (
-    <div>
+    <Layout location={location} title={data.site.siteMetadata.title}>
       <h1>{tagHeader}</h1>
-      <ul>
-        {edges.map(({ node }) => {
-          const { slug } = node.fields;
-          const { title } = node.frontmatter;
-          return (
-            <li key={slug}>
-              <Link to={slug}>{title}</Link>
-            </li>
-          );
-        })}
-      </ul>
+      <ol style={{ listStyle: "none" }}>
+        {posts.map(post => (
+          <PostListItem post={post} key={post.fields.slug} />
+        ))}
+      </ol>
       <Link to="/tags">All tags</Link>
-    </div>
+    </Layout>
   );
 };
 
@@ -32,20 +31,25 @@ export default Tags;
 
 export const pageQuery = graphql`
   query ($tag: String) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     allMarkdownRemark(
       limit: 2000
       sort: { frontmatter: { date: DESC } }
       filter: { frontmatter: { tags: { in: [$tag] } } }
     ) {
       totalCount
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-          }
+      nodes {
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          description
+          date(formatString: "MMMM DD, YYYY")
         }
       }
     }
