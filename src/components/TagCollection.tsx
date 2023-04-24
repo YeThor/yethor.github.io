42f40f37698cdb9da3f878848971197e35231c35
YESOL KIM <kysloves@naver.com>
2023-04-24 18:51:07 +0900

    :sparkles: add tag icon with pure css

--- src/components/TagCollection.tsx
+++ src/components/TagCollection.tsx
@@ -4,6 +4,7 @@ import { Link } from "gatsby";
 const TagCollection = ({ tags }) => {
   return (
     <div>
+      <i className="tag-icon" />
       {tags.map(tag => (
         <Link to={`/tags/${tag.toLowerCase()}`} className="tag" key={tag}>
           <span>{tag}</span>
