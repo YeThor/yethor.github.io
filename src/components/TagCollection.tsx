3eff015e2d1636de4aca44d51656ebe4a49392fb
YESOL KIM <kysloves@naver.com>
2023-05-05 22:38:25 +0900

    :sparkles: replace pure css tag icons with material icon

--- src/components/TagCollection.tsx
+++ src/components/TagCollection.tsx
@@ -4,7 +4,12 @@ import { Link } from "gatsby";
 const TagCollection = ({ tags }) => {
   return (
     <div>
-      <i className="tag-icon" />
+      <span
+        className="material-icons"
+        style={{ verticalAlign: "middle", marginRight: "0.3em" }}
+      >
+        label
+      </span>
       {tags.map(tag => (
         <Link to={`/tags/${tag.toLowerCase()}`} className="tag" key={tag}>
           <span>{tag}</span>
